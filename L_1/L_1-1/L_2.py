d7955f092bc2e676cfdc016bac8e3f642c940dc1
37255 <andrei3486@gmail.com>
2021-11-18 20:36:12 +0200

    new

---
+++ L_1/L_1-1/L_2.py
@@ -0,0 +1,6 @@
+a = float(input("Side A: "))
+c = float(input("Side B: "))
+b = float(input("Side C: "))
+
+half_p = (a + b + c) / 2
+
---
+++ L_1/tester.py
@@ -0,0 +1,20 @@
+a = 500
+b = 200
+
+# Addition '+'
+print(500+200)
+print(a + 200)
+print(a + b)
+
+# Subtraction '-'
+print(500 - 200)
+print(500 - b)
+print(a - b)
+
+print(500 * 200)
+
+a = 500
+b = 200
+
+print(a ** b)
+
