aa5d221b71b9e8538316f697dc020e109ff07e1a
37255 <andrei3486@gmail.com>
2021-11-30 16:05:43 +0200

    new

--- L_4/L4_2.py
+++ L_4/L4_2.py
@@ -63,10 +63,12 @@ people = [('Jack', 'Smith', 20, 'Tallinn', 'male'), ('Mary', 'Gold', 25, 'London
 #     elif num % 3 == 0:
 #         print(num, 'Fizz')
 #
-# chk1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
-# chk2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-# id = [3, 8, 6, 0, 6, 1, 8, 4, 2, 1, 4]
-# print(1 * 3 + 2 * 8 )
+chk1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+chk2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+id = [3, 8, 6, 0, 6, 1, 8, 4, 2, 1, 4]
+print((1 * 3 + 2 * 8 + 3 * 6 + 4 * 0 + 5 * 6 + 6 * 1 + 7 * 8 + 8 * 4 + 9 * 2 + 1 * 1) % 11)
+
+print((3 * 3 + 8 * 4 + 6 * 5 + 0 * 6 + 6 * 7 + 1 * 8 + 8 * 9 + 4 * 1 + 2 * 2 + 1 * 3) % 11)
 
 # while True:
 #     print('I can\'t stop')
@@ -90,14 +92,14 @@ people = [('Jack', 'Smith', 20, 'Tallinn', 'male'), ('Mary', 'Gold', 25, 'London
 #         condition = False
 #         print(user_input)
 
-distance_to_target = input('Please enter distance in meters: ')
-current_position = 0
-step = 0.5
-
-cnt = 0
-while current_position < int(distance_to_target):
-    cnt += 1
-    current_position += step
-
-print('Done')
-print(f'{cnt} steps')
+# distance_to_target = input('Please enter distance in meters: ')
+# current_position = 0
+# step = 0.5
+#
+# cnt = 0
+# while current_position < int(distance_to_target):
+#     cnt += 1
+#     current_position += step
+#
+# print('Done')
+# print(f'{cnt} steps')
